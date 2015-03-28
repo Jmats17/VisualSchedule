9ccd2889ed3b4b6927c44f3de2493e7caace3f9f
Jmats17 <Justin@Justins-MacBook-Air.local>
2015-03-28 15:34:28 -0400

    first commit

---
+++ VisualSchedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisualSchedule
+//
+//  Created by Justin Matsnev on 3/28/15.
+//  Copyright (c) 2015 Matsnev Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
