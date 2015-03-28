76c0fc8dc61b3652bf08371253113ed72c7198f2
Jmats17 <Justin@Justins-MacBook-Air.local>
2015-03-28 16:23:59 -0400

    button looks are good

--- VisualSchedule/ViewController.swift
+++ VisualSchedule/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
+        let buttonBorderColor = UIColor.blackColor()
         super.viewDidLoad()
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = 2
+        button.layer.borderColor = buttonBorderColor.CGColor
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func beginButton(sender: AnyObject) {
+  
+    }
 
 }
 
